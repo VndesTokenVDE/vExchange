68dfa2bd3ab7fb2dab361cd5aa1497cdf166e685
BHNinja <bhninjayt@gmail.com>
2023-04-15 23:48:36 +0500

    b

--- src/components/Exchange.jsx
+++ src/components/Exchange.jsx
@@ -49,7 +49,6 @@ const Exchange = () => {
   }
 
   return (
-
     <section id="exchange" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
       <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
       <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
@@ -59,7 +58,6 @@ const Exchange = () => {
         </span>
         <div className="w-full md:mt-0 mt-6">
           <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-            
             <label className='mt-2'>
               Amount you want to exchange:
               <input type="text" className={styles.inputField} placeholder="0"
